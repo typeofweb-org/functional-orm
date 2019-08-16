c72b7739f9894c4fdd26a66a316cc7545239fcec
Michal Miszczyszyn <michal@mmiszy.pl>
2019-08-16 17:00:12 +0200

    Jest now works with TS. Fixes #18

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,3 +1,13 @@
+import fc from 'fast-check';
+
+const contains = (pattern: string, text: string) => text.indexOf(pattern) !== -1;
+
 test('My Greeter', () => {
   expect(1).toBe(1);
+
+  fc.assert(
+    fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
+      expect(contains(b, a + b + c)).toBe(true);
+    }),
+  );
 });
